e5933bf57ad5fac9119389b22865a58b8ec7524d
ExpertYoyo666 <mefuga.yt@gmail.com>
2024-06-15 21:42:30 +0300

    added mains

---
+++ Client/VotingClient/Main.py
@@ -0,0 +1,15 @@
+import wx
+from Model import Model
+from MainFrame import MainFrame
+from Controller import Controller
+from Protocol import Protocol
+
+if __name__ == '__main__':
+    app = wx.App()
+    model = Model()
+    main_frame = MainFrame("Digital Voting System Voter Client")
+    main_frame.Show()
+    protocol = Protocol()
+    c = Controller(model, main_frame, protocol)
+
+    app.MainLoop()
