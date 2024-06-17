6b938e166ef3195272796abdd8e2949f7ec218f4
Erez Steinberg <erez.s@cipia.com>
2024-06-17 11:39:49 +0300

    Rename to Main.py

--- Server/Main.py
+++ Server/Main.py
@@ -1,7 +1,12 @@
 from DAL import DAL
 from Server import Server
 
-if __name__ == '__main__':
+
+def main():
     dal = DAL()
     server = Server(dal)
     server.main_loop()
+
+
+if __name__ == "__main__":
+    main()
